3fd06b69e47782c4d53dbfe47da0a5719f559d1f
Alexander Petros <contact@alexpetros.com>
2024-08-18 19:45:45 -0400

    Move routes to dedicated file

--- buzzer.go
+++ buzzer.go
@@ -5,21 +5,15 @@ import "fmt"
 import "net/http"
 import "os"
 
-import "buzzer/router/index"
-import "buzzer/router/healthcheck"
+import "buzzer/router"
 
 const ADDRESS = "localhost:3000"
 
 func main() {
-
   proxy_port := os.Getenv("BUZZER_PROXY_PORT")
+
   mux := http.NewServeMux()
-
-  // TODO static assets
-  // https://templ.guide/commands-and-tools/live-reload-with-other-tools#serving-static-assets
-
-  mux.HandleFunc("GET /", index.Get)
-  mux.HandleFunc("GET /healthcheck", healthcheck.Get)
+  router.SetupRouter(mux)
 
   if proxy_port != "" {
     fmt.Printf("Now listening at http://localhost:%s\n", proxy_port)
--- router/router.go
+++ router/router.go
@@ -1,3 +1,15 @@
 package router
 
+import "net/http"
 
+import "buzzer/router/index"
+import "buzzer/router/healthcheck"
+
+func SetupRouter (mux *http.ServeMux) {
+
+  // TODO static assets
+  // https://templ.guide/commands-and-tools/live-reload-with-other-tools#serving-static-assets
+
+  mux.HandleFunc("GET /{$}", index.Get)
+  mux.HandleFunc("GET /healthcheck", healthcheck.Get)
+}
