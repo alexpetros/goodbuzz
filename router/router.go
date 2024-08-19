476f337ac5c6cf2e35e5b9f2f161a38a7a50b6e9
Alexander Petros <contact@alexpetros.com>
2024-08-19 13:20:48 -0400

    Add rooms

--- router/router.go
+++ router/router.go
@@ -5,7 +5,8 @@ import "embed"
 
 import "buzzer/router/index"
 import "buzzer/router/healthcheck"
-import "buzzer/router/tournament"
+import "buzzer/router/tournaments"
+import "buzzer/router/rooms"
 
 // this bit of go magic embeds everything in the /static directory
 //go:embed all:static
@@ -16,7 +17,8 @@ func SetupRouter (mux *http.ServeMux) {
   mux.Handle("/static/", http.FileServer(http.FS(content)))
 
   mux.HandleFunc("GET /{$}", index.Get)
-  mux.HandleFunc("GET /tournaments/{id}", tournament.Get)
+  mux.HandleFunc("GET /tournaments/{id}", tournaments.Get)
+  mux.HandleFunc("GET /rooms/{id}", rooms.Get)
 
   mux.HandleFunc("GET /healthcheck", healthcheck.Get)
 }
