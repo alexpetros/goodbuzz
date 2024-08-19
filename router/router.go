7bb37de061842913ec6272096964828303674ba4
Alexander Petros <contact@alexpetros.com>
2024-08-19 11:50:17 -0400

    Add static files

--- router/router.go
+++ router/router.go
@@ -1,14 +1,18 @@
 package router
 
 import "net/http"
+import "embed"
 
 import "buzzer/router/index"
 import "buzzer/router/healthcheck"
 
+// this bit of go magic embeds all the static files
+//go:embed all:static
+var content embed.FS
+
 func SetupRouter (mux *http.ServeMux) {
 
-  // TODO static assets
-  // https://templ.guide/commands-and-tools/live-reload-with-other-tools#serving-static-assets
+  mux.Handle("/static/", http.FileServer(http.FS(content)))
 
   mux.HandleFunc("GET /{$}", index.Get)
   mux.HandleFunc("GET /healthcheck", healthcheck.Get)
