f16dfcb64a3c38e8bec808858f6804f7f45ba204
Alexander Petros <contact@alexpetros.com>
2024-10-18 18:33:08 -0400

    Add setup script

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ The app shows you everyone who's in the room with you, and doesn't require a log
 
 It's not yet hosted publicly, but it will be once I make a couple modifications to support that.
 
-## Installation
+## Developer Setup
 
 This uses [templ](https://github.com/a-h/templ) for templating and [air](https://github.com/air-verse/air) for live-reloading development.
 Follow the installation instructions for both.
@@ -23,3 +23,10 @@ Once these are installed, use the makefile to build and run the program:
 
 I found the hot-reloading somewhat problematic, and often found myself just using `make dev`.
 
+## Server Installation
+
+Included with the source is a shell script, `./ubuntu-vps-setup.sh`, that sets up the program on a Virtual Private Server (VPS) running Ubuntu.
+I use DigitalOcean, but as long as you're on the LTS Ubuntu version indicated in the script, you should be fine.
+
+To install it on a fresh VPS, run `cat ./ubuntu-vps-setup.sh | ssh roo@YOUR_VPS_IP`.
+The script will great a `goweb` user that will run the application, and copy the `root` public keys so that anyone who can ssh into `root` can ssh to `goweb`.
---
+++ ubuntu-vps-setup.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+#
+# Note that this needs to be run as root
+# Tested for Ubuntu 24.04 (LTS) x64
+
+set -veuo pipefail
+
+export DEBIAN_FRONTEND="noninteractive"
+
+export RUNAS_USER="goweb"
+export SERVICE_NAME="goodbuzz"
+export EXECUTEABLE_NAME="goodbuzz"
+export PACKAGE_URL="https://github.com/alexpetros/goodbuzz"
+
+# Create server user; note that this is a passwordless account
+useradd -m -s /bin/bash $RUNAS_USER
+usermod -aG sudo $RUNAS_USER
+rsync --archive --chown=$RUNAS_USER:$RUNAS_USER ~/.ssh /home/$RUNAS_USER
+
+# Install packages
+apt-get -y install nginx golang make sqlite3
+snap install --classic certbot
+ln -s /snap/bin/certbot /usr/bin/certbot
+
+# Setup firewall to only allow nginx and ssh access
+ufw allow 'OpenSSH'
+ufw allow 'Nginx Full'
+ufw --force enable
+ufw status
+
+# Download basic redirect conf and link it to sites-enabled
+cat > /etc/nginx/sites-available/$SERVICE_NAME <<EOF
+server {
+  server_name example.com;
+
+  location / {
+    proxy_pass http://localhost:9090;
+    proxy_set_header Connection '';
+    proxy_http_version 1.1;
+
+    # These are necessary for Server-Sent Events
+    # Comment them out if you don't need them
+    chunked_transfer_encoding off;
+    proxy_buffering off;
+    proxy_cache off;
+  }
+}
+EOF
+
+ln -s /etc/nginx/sites-available/$SERVICE_NAME /etc/nginx/sites-enabled/$SERVICE_NAME
+rm /etc/nginx/sites-enabled/default
+systemctl restart nginx
+
+# Verify that nginx is serving on port 80
+systemctl status nginx --no-pager --full
+
+# curl localhost:80 2>/dev/null | grep nginx > /dev/null
+
+# # Disable password authentication on ssh (yes I'm using ed)
+# ed /etc/ssh/sshd_config << EOF
+# %s/^PasswordAuthentication.*/PasswordAuthentication no
+# wq
+# EOF
+
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /home/$RUNAS_USER/.profile
+
+# Make log directory
+mkdir /var/log/$SERVICE_NAME
+
+# Setup go service for systemd
+cat > /lib/systemd/system/$SERVICE_NAME.service <<EOF
+[Unit]
+Description=$SERVICE_NAME
+
+[Service]
+WorkingDirectory=/home/$SERVICE_NAME
+Type=simple
+User=$RUNAS_USER
+Restart=always
+RestartSec=1s
+StandardOutput=append:/var/log/$SERVICE_NAME/output.log
+StandardError=append:/var/log/$SERVICE_NAME/error.log
+ExecStart=/home/$SERVICE_NAME/$EXECUTEABLE_NAME
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Login to $RUNAS_USER, set up the go environment, and install the program
+sudo -i -u $RUNAS_USER bash << EOF
+echo "PATH=$PATH:/home/$RUNAS_USER/go/bin" >> .profile
+source .profile
+cd /home/$RUNAS_USER
+git clone https://github.com/alexpetros/goodbuzz
+cd goodbuzz
+make install
+cat schema.sql | sqlite3 ../goodbuzz.db
+EOF
+
+systemctl restart goodbuzz
+
+echo "You're done!"
+echo "Don't forget you need to run: certbot --nginx -d example.com -d www.example.com"
+echo "You will need to 'go install' the service as the service user"
